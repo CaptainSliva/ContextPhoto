c5571b240f0926c3cbbe24a798ecb14d81fc574d
Captain Sliva <cripton2020@mail.ru>
2026-02-28 16:22:04 +0300

    change app icon

--- app/src/main/java/com/contextphoto/data/GlobalVariables.kt
+++ app/src/main/java/com/contextphoto/data/GlobalVariables.kt
@@ -2,8 +2,8 @@ package com.contextphoto.data
 
 import android.os.Environment
 
-const val PERMISSION_DELETE_REQUEST_CODE = 103
-val baseFilePath = "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)}/ContextPhoto/"
 const val versionDB = 1
+const val PERMISSION_DELETE_REQUEST_CODE = 103
 const val commentDatabase = "Context_photo_comment_database"
+val baseFilePath = "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)}/ContextPhoto/"
 val baseCommentsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
